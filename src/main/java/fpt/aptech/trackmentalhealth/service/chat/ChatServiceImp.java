6328612daeff28b2e0613ec90ca4adfffdb0edea
P52 <dangminhquan9320@gmail.com>
2025-06-09 22:50:05 +0700

    mquan created folders chat

---
+++ src/main/java/fpt/aptech/trackmentalhealth/service/chat/ChatServiceImp.java
@@ -0,0 +1,47 @@
+package fpt.aptech.trackmentalhealth.service.chat;
+
+import fpt.aptech.trackmentalhealth.entities.ChatMessage;
+import fpt.aptech.trackmentalhealth.entities.ChatSession;
+import fpt.aptech.trackmentalhealth.repository.ChatRepository.ChatMessagesRepository;
+import fpt.aptech.trackmentalhealth.repository.ChatRepository.ChatSessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChatServiceImp implements ChatService {
+
+    @Autowired
+    ChatMessagesRepository chatMessagesRepository;
+
+    @Autowired
+    ChatSessionRepository chatSessionRepository;
+
+
+    @Override
+    public List<ChatSession> getChatSessions() {
+        return List.of();
+    }
+
+    @Override
+    public ChatSession getChatSessionById(int id) {
+        return chatSessionRepository.getChatSessionById(id);
+    }
+
+
+    @Override
+    public List<ChatMessage> getChatMessagesByChatSessionId(int id) {
+        return chatMessagesRepository.getChatMessagesByIdSession(id);
+    }
+
+    @Override
+    public ChatMessage sendMessage(ChatMessage message) {
+        return  chatMessagesRepository.save(message);
+    }
+
+    @Override
+    public List<ChatSession> getChatSessionsByPsyId(int PsyId) {
+        return chatSessionRepository.getChatSessionByPsyId(PsyId);
+    }
+}
