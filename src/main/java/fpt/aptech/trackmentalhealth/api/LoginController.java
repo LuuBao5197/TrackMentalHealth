fe536735aef35331f0b5f5c3019a0ec9e5f8c11d
quy255 <149502973+quy255@users.noreply.github.com>
2025-07-18 12:54:22 +0700

    save

--- src/main/java/fpt/aptech/trackmentalhealth/api/LoginController.java
+++ src/main/java/fpt/aptech/trackmentalhealth/api/LoginController.java
@@ -202,7 +202,7 @@ public class LoginController {
     }
 
     @GetMapping("/profile/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+//    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getProfile(@PathVariable Integer id) {
         Optional<Users> optionalUser = loginRepository.findById(id);
         if (optionalUser.isEmpty()) {
