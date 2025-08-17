bd273428551d0a1d79b763857b8e8cb8999f8c3b
LuuBao5197 <baolpb5197@gmail.com>
2025-08-17 14:39:56 +0700

    Update code before pull

---
+++ src/main/java/fpt/aptech/trackmentalhealth/dto/test/history/UserTestDetailDTO.java
@@ -0,0 +1,19 @@
+package fpt.aptech.trackmentalhealth.dto.test.history;
+
+import fpt.aptech.trackmentalhealth.dto.test.OptionDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTestDetailDTO {
+    private String questionInstruction;
+    private String questionText;
+    private List<OptionDTO> options;
+    private String selectedOptionText;
+    private Integer scoreValue;
+}
